ee8a39c1e2b28062453c3f7aa123acf79c0c3b96
Adrian Bautista <adrianbautistaorozco@gmail.com>
2025-08-13 17:05:59 -0500

    Fix RFC cfdi import v3

--- app/Imports/PlainDataImport.php
+++ app/Imports/PlainDataImport.php
@@ -33,79 +33,78 @@ class PlainDataImport implements OnEachRow, WithHeadingRow, WithChunkReading, Sh
     {
         $row = $row->toArray();
 
-        if($row['uuid']) {
-            $employee = Employee::firstOrCreate(
-                ['social_number' => $row['numseguridadsocial']],
-                [
-                    'name' => $row['nombrereceptor'] ?? 'N/A',
-                    'clave' => $row['numempleado'] ?? 'N/A',
-                    'puesto' => $row['pueston'] ?? 'N/A',
-                    'depto' => $row['departamento'] ?? 'N/A',
-                    'curp' => '' ?? 'N/A',
-                    'rfc' => '--',
-                    'age' => '',
-                    'start_date' => $row['fechainiciorellaboral'] ?? 'N/A',
-                    'number' => $row['numempleado'] ?? 'N/A',
-                    'social_number' => $row['numseguridadsocial'] ?? 'N/A',
-                    'base_salary' => $row['salariobasecotapor'] ?? 'N/A',
-                    'daily_salary' => $row['salariobasecotapor'] ?? 'N/A',
-                    'company_id' => $this->company->id,
-                ]
-            );
+        $employee = Employee::firstOrCreate(
+            ['social_number' => $row['numseguridadsocial']],
+            [
+                'name' => $row['nombrereceptor'],
+                'clave' => $row['numempleado'],
+                'puesto' => $row['pueston'] ?? 'N/A',
+                'depto' => $row['departamento'] ?? 'N/A',
+                'curp' => '',
+                'age' => '',
+                'rfc' => $row['rfc_receptor'],
+                'start_date' => $row['fechainiciorellaboral'],
+                'number' => $row['numempleado'],
+                'social_number' => $row['numseguridadsocial'],
+                'base_salary' => $row['salariobasecotapor'],
+                'daily_salary' => $row['salariobasecotapor'],
+                'company_id' => $this->company->id,
+            ]
+        );
 
-            if ($row['tiponomina'] == 'O - Ordinaria' || $row['tiponomina'] == 'O') {
-                $year = $this->year;
-                $month = $this->getMonthFromPeriod($row['periodo']);
+        if ($row['tiponomina'] == 'O - Ordinaria' || $row['tiponomina'] == 'O') {
+            $year = $this->year;
+            $month = $this->getMonthFromPeriod($row['periodo']);
 
-                $carbon = new Carbon("last day of $month $year");
-                $start_date = new Carbon($employee->start_date);
-                $age = $carbon->diff($start_date);
+            $carbon = new Carbon("last day of $month $year");
+            $start_date = new Carbon($employee->start_date);
+            $age = $carbon->diff($start_date);
 
-                $vacation = $this->getVacation($age->y);
-                if (!$vacation) {
-                    // Maneja si no hay vacaciones definidas para ese año
-                    return;
-                }
+            $vacation = $this->getVacation($age->y);
+            if (!$vacation) {
+                // Maneja si no hay vacaciones definidas para ese año
+                return;
+            }
 
-                $daily_bonus = round($row['salariobasecotapor'] * $this->company->vacation_days / 365, 2);
-                $vacations_import = $row['salariobasecotapor'] * $vacation->days;
-                $vacation_bonus = round($vacations_import * ($this->company->vacation_bonus / 100) / 365, 2);
+            $daily_bonus = round($row['salariobasecotapor'] * $this->company->vacation_days / 365, 2);
+            $vacations_import = $row['salariobasecotapor'] * $vacation->days;
+            $vacation_bonus = round($vacations_import * ($this->company->vacation_bonus / 100) / 365, 2);
 
-                $yearUma = ((int) $row['periodo'] === 1) ? $year - 1 : $year;
-                $sdi_tope = Uma::where('year', $yearUma)->first();
-                $sdi_tope = $sdi_tope->balance * 25;
+            $yearUma = ((int) $row['periodo'] === 1) ? $year - 1 : $year;
+            $sdi_tope = Uma::where('year', $yearUma)->first();
+            $sdi_tope = $sdi_tope->balance * 25;
 
-                $sdi = round($row['salariobasecotapor'] + $daily_bonus + $vacation_bonus, 2);
+            $sdi = round($row['salariobasecotapor'] + $daily_bonus + $vacation_bonus, 2);
 
-                $salary = EmployeeSalary::where('year', $year)
-                    ->where('period', $row['periodo'])
-                    ->where('employee_id', $employee->id)
-                    ->first();
+            $salary = EmployeeSalary::where('year', $year)
+                ->where('period', $row['periodo'])
+                ->where('employee_id', $employee->id)
+                ->first();
 
-                $salaryData = [
-                    'period' => $row['periodo'],
-                    'year' => $year,
-                    'employee_id' => $employee->id,
-                    'category_id' => 1,
-                    'daily_salary' => $row['salariobasecotapor'],
-                    'daily_bonus' => $daily_bonus,
-                    'vacations_days' => $vacation->days,
-                    'vacations_import' => $vacations_import,
-                    'vacation_bonus' => $vacation_bonus,
-                    'sdi' => $sdi,
-                    'total_sdi' => 0,
-                    'sdi_limit' => $sdi_tope,
-                    'company_id' => $this->company->id,
-                    'variables' => '{}',
-                ];
+            $salaryData = [
+                'period' => $row['periodo'],
+                'year' => $year,
+                'employee_id' => $employee->id,
+                'category_id' => 1,
+                'daily_salary' => $row['salariobasecotapor'],
+                'daily_bonus' => $daily_bonus,
+                'vacations_days' => $vacation->days,
+                'vacations_import' => $vacations_import,
+                'vacation_bonus' => $vacation_bonus,
+                'sdi' => $sdi,
+                'total_sdi' => 0,
+                'sdi_limit' => $sdi_tope,
+                'company_id' => $this->company->id,
+                'variables' => '{}',
+            ];
 
-                if (!$salary) {
-                    EmployeeSalary::create($salaryData);
-                } else {
-                    $salary->update($salaryData);
-                }
+            if (!$salary) {
+                EmployeeSalary::create($salaryData);
+            } else {
+                $salary->update($salaryData);
             }
         }
+
     }
 
     protected function getVacation($years)
